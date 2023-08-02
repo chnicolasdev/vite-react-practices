9b9f5826ab586dc8d6bd64f28d5317f57cdf7254
chnicolasdev <christian.cnod@gmail.com>
2023-08-02 17:33:30 -0400

    Consumiendo API FakeStore Platzi

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,36 @@
+import { useState, useEffect } from 'react'
 import Layout from '../../Components/Layout'
 import Card from '../../Components/Card'
 
 const Home = () => {
 
+  const [items, setItems] = useState(null)
+
+  try {
+    // Se va a la API
+    useEffect(() => {
+      fetch('https://api.escuelajs.co/api/v1/products')
+        .then(response => response.json())
+        .then(data => setItems(data))
+    }, [])
+  } catch (error) {
+    console.log(error);
+  }
+
   return (
     <Layout>
       Home
-      <Card />
+      <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+        {
+          // Se cambian las llaves por parentesis para que haga un return
+          items?.map((item) => (
+            <Card 
+              key={item.id}
+              data={item}
+            />
+          )) 
+        }
+      </div>
     </Layout>
   )
 }
